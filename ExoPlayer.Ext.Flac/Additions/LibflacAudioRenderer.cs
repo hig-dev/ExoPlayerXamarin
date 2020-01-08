0f71c870a43cfb86d21aa668d176da62712db716
Halil <hig@outlook.com>
2020-01-09 00:29:34 +0100

    Fix flac extension binding error

---
+++ ExoPlayer.Ext.Flac/Additions/LibflacAudioRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Com.Google.Android.Exoplayer2.Decoder;
+using Com.Google.Android.Exoplayer2.Drm;
+using Java.Interop;
+
+namespace Com.Google.Android.Exoplayer2.Ext.Flac
+{
+    public partial class LibflacAudioRenderer : global::Com.Google.Android.Exoplayer2.Audio.SimpleDecoderAudioRenderer
+    {
+        /// <summary>
+        /// This method just exists to solve building errors of the binding project, so don't use it.
+        /// </summary>
+        /// <returns>null</returns>
+        protected override SimpleDecoder CreateDecoder(Format format, IExoMediaCrypto mediaCrypto)
+        {
+            return null;
+        }
+    }
+}
